6fb5b802a79e9478f8f9bcbec869eac08f55d05b
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-14 21:21:04 -0300

    eslint prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Route path="/" element={<Profile />} />
+      <Route path="/:username" element={<Profile />} />
+      <Route path="/:username/:reponame" element={<Repo />} />
+
+      {/* <Footer /> */}
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
