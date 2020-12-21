c0da13daa1a6b6436070555016c06fdecc33356b
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-21 17:15:03 -0300

    fix date

--- src/components/RandomCalendar/styles.ts
+++ src/components/RandomCalendar/styles.ts
@@ -30,4 +30,13 @@ export const Container = styled.div`
 
     width: 893px;
   }
+
+  span {
+    font-size: 11px;
+    color: var(--link);
+    margin-top: -25px;
+    margin-left: 7px;
+    padding-bottom: 16px;
+    align-self: flex-start;
+  }
 `;
