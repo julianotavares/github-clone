acd7a3c1acc3dfc382dce63c7565ad60d81ffd66
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-22 16:47:25 -0300

    Fix header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -13,24 +13,24 @@ export const GithubLogo = styled(FaGithub)`
   width: 32px;
   height: 32px;
   flex-shrink: 0;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 export const SearchForm = styled.form`
   padding-left: 16px;
   width: 100%;
-
   input {
     background: var(--search);
     outline: 0;
     border-radius: 6px;
     padding: 7px 12px;
-
     width: 100%;
-
-    &:hover {
+    &:focus {
       width: 318px;
     }
-
     transition: width 0.2s ease-out, color 0.2s ease-out;
   }
 `;
